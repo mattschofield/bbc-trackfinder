604153e4f278e03fa74d142bbbcc14fca8abed5d
Sara Gozalo - FM and AM <gozals01@mc-s083181.national.core.bbc.co.uk>
2014-02-12 12:06:47 +0000

    text working

--- Radio2Cloud/cloud/app.js
+++ Radio2Cloud/cloud/app.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var app = express();
 
+
 // Global app configuration section
 app.set('views', 'cloud/views');  // Specify the folder to find templates
 app.set('view engine', 'ejs');    // Set the template engine
--- Radio2Cloud/cloud/main.js
+++ Radio2Cloud/cloud/main.js
@@ -1,6 +1,55 @@
-  require('cloud/app.js');
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+var twilio = require('twilio')('AC6fce2fb8441b4279897882f82ad80ed3', 'c1805ed5bd9fc3a8d17a200688421600');
+var twiliolib = require('twilio');
+require('cloud/app.js');
+ 
+ 
+Parse.Cloud.define("receiveSMS", function(request, response) {
+    console.log("Received a new text: " + request.params.From);
+ 
+    var id = request.params.id;
+    var numberTo = request.params.From;
+ 
+    xmasMixer.getRemix(numberTo, id, response);
+ 
+     
+});
+ 
+Parse.Cloud.define("radio2", function(request, response) {
+    console.log("Received a new text: " + request.params.From);
+ 
+    var body = request.params.Body;
+    var numberTo = request.params.From;
+    if(body.toUpperCase().indexOf("RADIO2") != -1){
+    	twilio.sendSms({
+            to: numberTo, 
+            from: '+441633538987', 
+            body: 'YAY'
+        }, function(err, responseData) { 
+            if (err) {
+              console.log(err);
+            } else { 
+              console.log(responseData.from); 
+              console.log(responseData.body);
+            }
+          }
+        );
+     }else{
+     	twilio.sendSms({
+            to: numberTo, 
+            from: '+441633538987', 
+            body: 'wrong entry'
+        }, function(err, responseData) { 
+            if (err) {
+              console.log(err);
+            } else { 
+              console.log(responseData.from); 
+              console.log(responseData.body);
+            }
+          }
+        );
+        
+     }
+     response.success();
+ 
+     
 });
