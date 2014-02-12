5beccf0ddf9af0e614e188c3d01f78e6c6e91401
Matt Schofield <matt.schofield.102@gmail.com>
2014-02-12 14:38:53 +0000

    Simple parsing of static file for testing

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
+var fs = require('fs')
+  , path = require('path');
 
 /*
  * GET home page.
  */
 
+var tempResults = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/results.json')))
+
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', results: tempResults.results, log: console.log });
 };
